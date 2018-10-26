b4d38bbc8266fada4f3fcb465a5cb785cbe01c96
Bhautik Vasoya <bavasoya@gmail.com>
2018-10-26 23:18:30 +0530

    [ADD]Initialize Free Module

---
+++ enlarged_search_area_akili/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Enlarged search area',
+    'summary': """This module will allow the user to enlarge the search box area.""",
+    'description': """This module is helpful in increasing the Search Enlarged Area."""
+    'version': '10.0.1.0.0',
+    'category': 'Website',
+    'depends': ['website_sale']
+    'company': 'Akili Systems Pvt. Ltd.',
+    'author': 'Akili Systems Pvt Ltd',
+    'website': 'http://www.akilisystems.in',
+
+    'data': [
+        'views/templates.xml',
+        'views/assets.xml',
+    ],
+    'images': [
+        'static/description/sicon.png',
+    ],
+
+    'installable': True,
+    'application': True,
+}
---
+++ remove_product_akili/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Remove product',
+    'version': '10.0.1.0.0',
+    'depends': ['website_sale']
+    'author': 'Akili Systems Pvt. LTD.',
+    'category': 'ecommerce',
+    'summary': """This module will allow the user to remove the product in the cart.""",
+    'description': """This module has the functionality of remove button in the cart.""",   
+    'company': 'Akili Systems Pvt. LTD.',
+    'website': 'http://www.akilisystems.com',
+    'data': [ 
+        'views/templates.xml'
+        ],
+    'image':[
+         'static/description/active.png',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
